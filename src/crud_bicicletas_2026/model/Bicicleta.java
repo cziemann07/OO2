8b3ad094d414af7742f7ba146db8b79fb5013094
green-code-labs <suporte@greencodelabs.com>
2026-05-11 13:30:07 -0300

    first commit

---
+++ src/crud_bicicletas_2026/model/Bicicleta.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crud_bicicletas_2026.model;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author cadul
+ */
+public class Bicicleta {
+    private int id;
+    private String marca;
+    private String modelo;
+    private String tipo;
+    private int numeroMarchas;
+    private double aro;
+    private BigDecimal preco;
+    private int anoFabricacao;
+    
+    public Bicicleta(){}
+    
+    public Bicicleta(int id, String marca, String modelo, String tipo, int numeroMarchas, double aro, BigDecimal preco, int anoFabricacao) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipo = tipo;
+        this.numeroMarchas = numeroMarchas;
+        this.aro = aro;
+        this.preco = preco;
+        this.anoFabricacao = anoFabricacao;
+    }
+    
+    public int getId() { 
+        return id; 
+    }
+    public void setId(int id) { 
+        this.id = id; 
+    }
+
+    public String getMarca() { 
+        return marca; 
+    }
+    public void setMarca(String marca) { 
+        this.marca = marca; 
+    }
+
+    public String getModelo() { 
+        return modelo; 
+    }
+    public void setModelo(String modelo) { 
+        this.modelo = modelo; 
+    }
+
+    public String getTipo() { 
+        return tipo; 
+    }
+    public void setTipo(String tipo) { 
+        this.tipo = tipo; 
+    }
+
+    public int getNumeroMarchas() { 
+        return numeroMarchas; 
+    }
+    public void setNumeroMarchas(int numeroMarchas) { 
+        this.numeroMarchas = numeroMarchas; 
+    }
+
+    public double getAro() { 
+        return aro; 
+    }
+    public void setAro(double aro) { 
+        this.aro = aro; 
+    }
+
+    public BigDecimal getPreco() { 
+        return preco; 
+    }
+    public void setPreco(BigDecimal preco) { 
+        this.preco = preco; 
+    }
+
+    public int getAnoFabricacao() { 
+        return anoFabricacao; 
+    }
+    public void setAnoFabricacao(int anoFabricacao) { 
+        this.anoFabricacao = anoFabricacao; 
+    }
+
+    @Override
+    public String toString() {
+        return marca + " " + modelo + " (Aro " + aro + ")";
+    }
+}
---
+++ src/crud_bicicletas_2026/util/GerenciadorConexao.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crud_bicicletas_2026.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author cadul
+ */
+public class GerenciadorConexao {
+    private static final String URL = "jdbc:postgresql://localhost:5432/bicicletaria";
+    private static final String user = "admin";
+    private static final String pass = "admin";
+    
+    public static Connection getConexao() throws SQLException {
+        return DriverManager.getConnection(URL, user, pass);
+    }
+    
+    public static void fecharConexao(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar conexão: " + e.getMessage());
+            }
+        }
+    }
+}
